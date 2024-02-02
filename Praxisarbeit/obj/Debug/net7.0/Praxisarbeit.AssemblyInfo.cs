2b6f9b2513c5b336da5715999a69d01055500afa
Davud-Ponjevic <davud.ponjevic@outlook.com>
2024-02-02 10:00:20 +0100

    Postman

--- Praxisarbeit/obj/Debug/net7.0/Praxisarbeit.AssemblyInfo.cs
+++ Praxisarbeit/obj/Debug/net7.0/Praxisarbeit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Praxisarbeit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e47313d5b2c739a9d2a095283bc846d541855d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8c6532de187897014ec821065904f52d74b957")]
 [assembly: System.Reflection.AssemblyProductAttribute("Praxisarbeit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Praxisarbeit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
